2d2c084a8a3af8f42a0d83ffc050aa5eec3ad176
kpgraham <kpgraham@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-14 15:16:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stop-spammer-registrations-plugin/trunk@289396 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,19 +1,27 @@
 === Stop Spammer Registrations Plugin ===
-Tags: spam, registration, spammers
+Tags: spam, registration, spammers, MU
 Donate link: https://online.nwf.org/site/Donation2?df_id=6620&6620.donation=form1
 Requires at least: 2.3
 Tested up to: 3.0
 Contributors: Keith Graham
-Stable tag: 1.3
+Stable tag: 1.4
 
-Uses the StopForumSpam.com database to validate emails and deny registration to spammers.
+This plugin Uses the StopForumSpam.com DB to prevent spammers from registering or making comments.
 
 == Description ==
-The Stop Spammer Registrations Plugin Plugin Accesses the StopForumSpam.com db to check emails before a user can register. The StopForumSpam database contains over a million spammer emails and is updated daily. 
+The Stop Spammer Registrations Plugin Plugin Accesses the StopForumSpam.com db to check emails, username and IP before a user can register. The StopForumSpam database contains over a million spammer emails, User Names and IPs, and is updated daily. 
 
-Many spam solutions are concerned with comments. This plugin validates email addresses and is primarily concerned with registrations. I have a website where I needed to allow registrations, but the users would probably not comment. This plugin kept the flood of registrations down to a manageable few.
+The plugin validates Email, IP and User name against the StopForumSpam.com db. It denies login attempts, such as spammers trying to guess passwords or spammers trying to create login ids. It also stops spammers who try to add comments with a spammer email or ip addresses.
 
-A list of the last 20 emails denied access is included under the plugin setup page.
+The plugin caches 60 spam results to avoid hits on StopForumSpam.com. It remembers the Username, IP and Email of any request that fails on any part of Username, IP and Email, so be careful when testing because an invalid Email will also invalidate your IP or test username. You may have to clear the cache in order to clear out a mistake.
+
+StopForumSpam.com limits hits on its database to 5,000 a day, so if you are being hit hard by spammers you may not be able to effectively block them. I have yet to see this many hits on any of my sites. 
+
+Many spam solutions are concerned with comments. This plugin validates email addresses and is primarily concerned with registrations. I have a website where I needed to allow registrations, but the users would probably not comment. This plugin kept the flood of registrations down to a manageable few. It works when Wordpress Validates an email address so it will also work when users enter an email address in comments.
+
+This plugin also stopped spam registations on my WordPress MU site.
+
+The plugin dispays a list of the last 30 emails, IPs, User names and passwords used in failed denied login attempts. It displays the cached results of hits to StopForumSpam.com up to 60 entries.
 
  
 == Installation ==
@@ -31,6 +39,9 @@ A list of the last 20 emails denied access is included under the plugin setup pa
  
 = 1.3 =
  * Check the ip address whenever email is checked.
+ 
+= 1.4 =
+ * Checks the user name. Cache failed attempts with option to clear cache. Cleans up after itself when uninstalled. 
  
 
 == Support ==
--- stop-spammer-registrations.php
+++ stop-spammer-registrations.php
@@ -3,7 +3,7 @@
 Plugin Name: Stop Spammer Registrations Plugin
 Plugin URI: http://www.BlogsEye.com/
 Description: Uses the Stop Forum Spam DB to prevent spammers from registering
-Version: 1.3
+Version: 1.4
 Author: Keith P. Graham
 Author URI: http://www.BlogsEye.com/
 
@@ -18,6 +18,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 *
 *************************************************************/
 function kpg_stop_sp_reg_fixup($email) {
+	if (empty($email)) return $email;
 	// this is the Stop Spammer Registrations functionality.
 	// email as the email to validate
 	
@@ -31,54 +32,125 @@ function kpg_stop_sp_reg_fixup($email) {
 
 	*/
 	// get the options
-
+	$options=get_option('kpg_stop_sp_reg_options');
+	if (empty($options)) $options=array();
+	// cache bad cases
+	$badips=array();
+	$badems=array();
+	$badids=array();
+	$sphist=array();
+	$spcount=0;
+	if (array_key_exists('spcount',$options)) $spcount=$options['spcount'];
+	if (array_key_exists('sphist',$options)) $sphist=$options['sphist'];
+	if (array_key_exists('badems',$options)) $badems=$options['badems'];
+	if (array_key_exists('badips',$options)) $badips=$options['badips'];
+	if (array_key_exists('badids',$options)) $badids=$options['badids'];
+	
+	
 	// first check the ip address
 	$ip=$_SERVER['REMOTE_ADDR'];
-	$ipok=='';
-	if (!empty($ip)) {
-		$ipok=kpg_stop_sp_reg_getafile("http://www.stopforumspam.com/api?ip=$ip");
+	$username=''; // log
+	if (!empty($_POST)) {
+		// user_login usr_email 
+		if (array_key_exists('user_login',$_POST)) $username=$_POST['user_login'];
 	}
-	// fix the email 
+	//kpg_logit(" '$email', '$username', '$ip' \r\n"); // turn on only during debugging
+	
+	// check the data
+	$deny=false;
+	// build the check
 	$em=urlencode($email);
+	$username=urlencode($username);
 	$query="http://www.stopforumspam.com/api?email=$em";
-	$ansa=kpg_stop_sp_reg_getafile($query);
-	if ( empty($ansa) && empty($ipok) ) return $email;
-	if (strpos($ansa,'<appears>yes</appears>')||strpos($ipok,'<appears>yes</appears>')) {
-		// record the last few guys that have  tried to spam
-		// add the bad spammer to the history list
-		$options=get_option('kpg_stop_sp_reg_options');
-		if (empty($options)) $options=array();
-		$spcount=0;
-		if (array_key_exists('spcount',$options)) $spcount=$options['spcount'];
-		$sphist=array();
-		if (array_key_exists('sphist',$options)) $sphist=$options['sphist'];
-		$spcount++;
-		$sphist[count($sphist)]=htmlentities($email);
-		if (count($sphist)>30) array_shift($sphist);
-		$options['sphist']=$sphist;
-		$options['spcount']=$spcount;
-		update_option('kpg_stop_sp_reg_options', $options);
-		
-		return false;
+	if (!empty($ip)) {
+		$query=$query."&ip=$ip";
 	}
-
-	return $email;
+	if (!empty($username)) {
+		$query=$query."&username=$username";
+	}
+	// check to see if the results have been cached
+	$deny=false;
+	if (array_key_exists($em,$badems)) {
+		$badems[$em]=date("m/d/y h:i:s A");
+		$deny=true;
+	} 
+	if (array_key_exists($ip,$badips)) {
+		$badips[$ip]=date("m/d/y h:i:s A");
+		$deny=true;
+	} 
+	if (array_key_exists($username,$badids)) {
+		$badids[$username]=date("m/d/y h:i:s A");
+		$deny=true;
+	}
+	if (!$deny) {
+		$check=kpg_stop_sp_reg_getafile($query);
+		//kpg_logit(" '$query', '$check' \r\n"); // turn on only during debugging
+		if (strpos($check,'<appears>yes</appears>')) {
+			$deny=true;
+		} else {
+			$deny=false;
+		}
+	} else {
+		//kpg_logit(" found in cache \r\n");// turn on only during debugging
+	}
+	if (!$deny) return $email;
+	
+	// update the history files.
+	// record the last few guys that have  tried to spam
+	// add the bad spammer to the history list
+	$spcount++;
+	$sphist[count($sphist)]=$email.'|'.date("m/d/y h:i:s A").'|'.$ip.'|'.$username;
+	if (count($sphist)>30) array_shift($sphist);
+	$options['sphist']=$sphist;
+	$options['spcount']=$spcount;
+	// Cache the bad guy
+	$badems[$em]=date("m/d/y h:i:s A");
+	if (!empty($ip)) $badips[$ip]=date("m/d/y h:i:s A");
+	if (!empty($username)) $badids[$username]=date("m/d/y h:i:s A");
+	// sort the array by date so that the most recent date is last
+	arsort($badems);
+	arsort($badips);
+	arsort($badids);
+	// update the caches
+	if (count($badems)>60) array_pop($badems);
+	if (count($badips)>60) array_pop($badips);
+	if (count($badids)>60) array_pop($badids);
+	$options['badids']=$badids;
+	$options['badips']=$badips;
+	$options['badems']=$badems;
+	update_option('kpg_stop_sp_reg_options', $options);
+	
+	return false;
 	
 ?>
 
 <?php
 }
+
+
 function kpg_stop_sp_reg_control()  {
 // this is the display of information about the page.
-
+	if(!current_user_can('manage_options')) {
+		die('Access Denied');
+	}
+	if (array_key_exists('kpg_stop_clear',$_GET)) {
+		// clear the cache
+		$options=get_option('kpg_stop_sp_reg_options');
+		if (empty($options)) $options=array();
+		unset($options['badids']);
+		unset($options['badips']);
+		unset($options['badems']);
+		update_option('kpg_stop_sp_reg_options', $options);
+	}
 ?>
 
 <div class="wrap">
 <h2>Stop Spammer Registrations Plugin</h2>
 <h4>The Stop Spammer Registrations Plugin is installed and working correctly.</h4>
-<p>This plugin Uses the Stop Forum Spam DB to prevent spammers from registering.</p>
+<p>This plugin Uses the Stop Forum Spam DB to prevent spammers from registering or making comments.</p>
 <p>There are no configurations options. The plugin is on when it is installed and enabled. To turn it off just disable the plugin from the plugin menu.. </p>
-
+<p>If a registration is rejected because of a hit on the StopForumSpam.com db, this plugin caches the userid and IP. If you test the plugin using spammer credentials, it will remember that your IP address was associated with the spammer&apos;s email and deny future registrations from your IP. If you feel compelled to test the plugin, you may lock yourself out of comments and the registration form. If you do get into a problem where you have cached a valid IP, click here: <a href="<?php echo $_SERVER["REQUEST_URI"]; ?>&kpg_stop_clear=true">Clear the Cache.</a></p>
+<p>Note: StopForumSpam.com limits checks to 5,000 per day for each IP so the plugin may stop validating on very busy sites. I have not seen this happen, yet. Results are cached in order to thwart repeated attempts. You may see your own email in the cache as spammers try to use it to leave comments. You may have to clear the cache to use your own email in that case.</p>
 <hr/>
 <h3>Recent activity</h3)
 <?php
@@ -88,6 +160,13 @@ function kpg_stop_sp_reg_control()  {
 	if (array_key_exists('spcount',$options)) $spcount=$options['spcount'];
 	$sphist=array();
 	if (array_key_exists('sphist',$options)) $sphist=$options['sphist'];
+	$badips=array();
+	$badems=array();
+	$badids=array();
+	if (array_key_exists('badems',$options)) $badems=$options['badems'];
+	if (array_key_exists('badips',$options)) $badips=$options['badips'];
+	if (array_key_exists('badids',$options)) $badids=$options['badids'];
+
 	if (empty($spcount)||empty($sphist)) {
 		echo "<p>No activity Recorded.</p>";
 	} else {
@@ -95,13 +174,44 @@ function kpg_stop_sp_reg_control()  {
 	<p>Recent blocked email registration attempts</p>";
 	echo "<ul>";
 	for ($j=0;$j<count($sphist);$j++) {
-		$em=$sphist[$j];
-		echo "<li><a href=\"http://www.stopforumspam.com/search?q=$em\" target=\"_blank\">$em</a></li>";
+		$data=$sphist[$j];
+		$ln=explode('|',$data);
+		$em=trim($ln[0]);
+		$dt=$ln[1];
+		$ip=$ln[2];
+		$un=$ln[3];
+		$pw=$ln[4];
+		if (!empty($em)) {
+			echo "<li style=\"font-size:.8em;\"><a href=\"http://www.stopforumspam.com/search?q=$em\" target=\"_blank\">email: $em</a>";
+			if (!empty($dt)) echo "; Date: $dt";
+			if (!empty($ip)) echo "; <a href=\"http://www.stopforumspam.com/search?q=$ip\" target=\"_blank\">IP: $ip</a>";
+			if (!empty($un)) echo "; <a href=\"http://www.stopforumspam.com/search?q=$un\" target=\"_blank\">User Name: $un</a>";
+			echo "</li>";
+		}
 	}
 	echo "</ul>";
 	}
 
-?>	
+?>	<h3>Cached Values</h3>
+<table  >
+	<tr>
+	<td  style="border: 1px solid black;font-size:.75em;"><?php
+		foreach ($badems as $key => $value) {
+        echo "Email: $key; Date: $value<br/>\r\n";
+		}
+	?></td>
+	<td  style="border: 1px solid black;font-size:.75em;"><?php
+		foreach ($badips as $key => $value) {
+			echo "ip: $key; Date: $value<br/>\r\n";
+		}
+	?></td>
+	<td  style="border: 1px solid black;font-size:.75em;"><?php
+		foreach ($badids as $key => $value) {
+			echo "User name: $key; Date: $value<br/>\r\n";
+		}
+	?></td>
+	</tr>
+</table>
 <hr/>
 
 <p>This plugin is free and I expect nothing in return. However, a link on your blog to one of my personal sites would be appreciated.</p>
@@ -118,13 +228,25 @@ function kpg_stop_sp_reg_control()  {
 
 <?php
 }
-// no unistall because I have not created any meta data to delete.
+
+
 function kpg_stop_sp_reg_init() {
    add_options_page('Stop Spammer Registrations', 'Stop Spammer Registrations', 'manage_options',__FILE__,'kpg_stop_sp_reg_control');
 }
-  // Plugin added to Wordpress plugin architecture
+  
+function kpg_stop_sp_reg_uninstall() {
+	if(!current_user_can('manage_options')) {
+		die('Access Denied');
+	}
+	delete_option('kpg_stop_sp_reg_options'); 
+	return;
+}  
+
 add_filter('is_email','kpg_stop_sp_reg_fixup');	
 add_action('admin_menu', 'kpg_stop_sp_reg_init');
+if ( function_exists('register_uninstall_hook') ) {
+	register_uninstall_hook(__FILE__, 'kpg_stop_sp_reg_uninstall');
+}
 
 function kpg_stop_sp_reg_getafile($f) {
    // uses fopen or curl depending on "allow_url_fopen = On" being avaialble
@@ -144,6 +266,13 @@ function kpg_stop_sp_reg_getafile($f) {
 	curl_close($ch);
 	return $ansa;
 }
+// used only during debugging - please ignore the man behind the curtain.
+/* function kpg_logit($line) {
+	$f=fopen('log.txt','a');
+	fwrite($f,$line);
+	fclose($f);
+	return;
 
-
+}
+*/
 ?>
